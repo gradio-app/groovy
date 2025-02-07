3a2fd71e948f7db0d45259fd292bb67e2a76520a
Abubakar Abid <abubakar@huggingface.co>
2025-02-07 15:24:39 -0800

    changes

--- grompy/__init__.py
+++ grompy/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from grompy.transpiler import transpile
+
 __version__ = Path(__file__).parent.joinpath("version.txt").read_text().strip()
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "transpile"]
